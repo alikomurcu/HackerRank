c2bab1f27cbdfa145f8299730538a99ebb52880f
Ali <56913792+alikomurcu@users.noreply.github.com>
2020-03-08 01:33:52 +0300

    Create write_a_function.py

---
+++ write_a_function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    leap = False
+    if year%4 == 0:
+        if year%100 == 0:
+            leap = False
+            if year%400 == 0:
+                leap = True
+        else:
+            leap = True    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+""" Another solution here:
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
