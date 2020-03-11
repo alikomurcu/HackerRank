a51cccebda14e3bbc7c3a99cc4a7ea06a7935633
Ali <56913792+alikomurcu@users.noreply.github.com>
2020-03-11 17:49:43 +0300

    Update functions_in_c.c

--- functions_in_c.c
+++ functions_in_c.c
@@ -2,11 +2,7 @@
 
 int max_of_four(int a, int b, int c, int d){ /* Puts the greatest one to last index.*/
     int i, tmp;
-    int arr[4];
-    arr[0] = a;
-    arr[1] = b;
-    arr[2] = c;
-    arr[3] = d;
+    int arr[4] = {a,b,c,d};
     for(i = 0; i<3; i++){ /* Just the first step of bubble sort algorithm is implemented in order to find the greatest value.*/
         if(arr[i] > arr[i+1]){
             tmp = arr[i];
