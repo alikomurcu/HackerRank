bcf24dba2d83498d27a0c9772a995befb2c7ee1d
Ali <56913792+alikomurcu@users.noreply.github.com>
2020-03-11 17:37:13 +0300

    Create functions_in_c.c

---
+++ functions_in_c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d){ // Puts the greatest one to last index.
+    int i, tmp;
+    int arr[4] = {a, b, c, d};
+    for(i = 0; i<3; i++){ // Just the first step of bubble sort algorithm is implemented in order to find the greatest value.
+        if(arr[i] > arr[i+1]){
+            tmp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = tmp;
+            }
+    }
+    return arr[3];
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
